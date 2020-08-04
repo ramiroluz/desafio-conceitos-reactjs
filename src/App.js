8e12af7a915c7a0450bbf77b8cfe7f72dc8e4e75
Ramiro Batista da Luz <ramiroluz@gmail.com>
2020-08-04 20:31:03 -0300

    Implementa lista de repositorios. fix #5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from "react";
-
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+        setRepositories(response.data);
+    });
+}, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Novo repositorio ${Date.now()}`,
+      owner: 'Biroliro'
+  });
+
+  const repo = response.data;
+
+  setProjects([... repositories, repo]);
   }
 
   async function handleRemoveRepository(id) {
@@ -14,13 +29,11 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repo => <li key={repo.id}>{repo.title}
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
-        </li>
+          </li>)}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -18,6 +18,47 @@ const actWait = async (amount = 0) => {
 };
 
 describe("App component", () => {
+  it("should be able to list repositories", async () => {
+    const { getByText, getByTestId } = render(<App />);
+
+    apiMock.onGet("repositories").reply(200, [
+      {
+        id: "123",
+        url: "https://github.com/josepholiveira",
+        title: "josepholiveira",
+        techs: ["React", "Node.js"],
+      },
+      {
+        id: "234",
+        url: "https://github.com/josesimpsons",
+        title: "josesimpsons",
+        techs: ["React", "Node.js"],
+      },
+      {
+        id: "345",
+        url: "https://github.com/zesimpsons",
+        title: "zesimpsons",
+        techs: ["React", "Node.js"],
+      },
+      {
+        id: "456",
+        url: "https://github.com/marietasimpsons",
+        title: "marietasimpsons",
+        techs: ["React", "Node.js"],
+      }
+    ]);
+
+    await actWait();
+
+    expect(getByTestId("repository-list").textContent).toContain("josepholiveira");
+
+    expect(getByTestId("repository-list").textContent).toContain("josesimpsons");
+
+    expect(getByTestId("repository-list").textContent).toContain("zesimpsons");
+    
+    expect(getByTestId("repository-list").textContent).toContain("marietasimpsons");
+  });
+
   it("should be able to add new repository", async () => {
     const { getByText, getByTestId } = render(<App />);
 
